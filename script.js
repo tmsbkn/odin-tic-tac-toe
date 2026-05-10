5e60841796489f23c944228d608ded9f625f6d40
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-10 19:33:26 -0400

    Write to-do

---
+++ script.js
@@ -0,0 +1,9 @@
+// Attach event listener to boxes
+
+// initialize game
+
+//set win conditions
+
+// determine current player
+
+//after each move, check win conditions. If not met change active player
