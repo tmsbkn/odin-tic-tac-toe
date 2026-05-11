848995e79ee59cd0cf8e3371ab185f7041120861
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-10 20:40:22 -0400

    Add winningMoves

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const winningMoves = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+];
+
 const form = document.querySelector('#gameForm');
 
 form.addEventListener('submit', (event) => {
