105957337885040b8e0ed249a7e82e5b73e390bf
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-10 20:05:48 -0400

    Get form data

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const form = document.querySelector('#gameForm');
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+
+   const formData = new FormData(form);
+   const data = Object.fromEntries(formData);
+   document.querySelector('.formWrap').setAttribute('hidden', true);
+   console.log(data);
+});
 // Attach event listener to boxes
 
 // initialize game
