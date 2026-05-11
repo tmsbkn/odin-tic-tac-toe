162d1e3464cead0a0a5cffca49af30ee8252ce00
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-10 20:15:20 -0400

    Initialize game variables

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ form.addEventListener('submit', (event) => {
    const data = Object.fromEntries(formData);
    document.querySelector('.formWrap').setAttribute('hidden', true);
    console.log(data);
+   initializeGame(data);
 });
+
+const initializeGame = (data) => {
+   //initialize game variables
+
+   //add event click listeners to gameBoard
+   data.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+   data.player1 = 'X';
+   data.player2 = 'O';
+   data.round = 0;
+   data.currentPlayer = 'X';
+   data.gameOver = false;
+};
+
 // Attach event listener to boxes
 
 // initialize game
